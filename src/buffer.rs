a8abb0ae263052011afb4b8c5573fcf9f251ae12
supinie <86788874+supinie@users.noreply.github.com>
2023-08-15 10:56:19 +0100

    add compress poly

--- src/buffer.rs
+++ src/buffer.rs
@@ -71,16 +71,41 @@ impl Buffer {
     }
 
     // Compress polynomial to a buffer
-    // pub fn compress(&mut self, poly: Poly, compressed_bytes: usize) {
-    //     let mut k = 0usize;
-    //     let mut t = [0u8; 8];
+    pub fn compress(&mut self, poly: Poly, compressed_bytes: usize) {
+        let mut k = 0usize;
+        let mut t = [0u8; 8];
 
-    //     match compressed_bytes {
-    //         128 => {
-    //             for i in 0..N / 8 {
-    //                 for j in 0..8 {
-    //                     let mut u = poly.coeffs[8 * i + j];
-                
-
-
+        match compressed_bytes {
+            128 => {
+                for i in 0..N / 8 {
+                    for j in 0..8 {
+                        let mut u = poly.coeffs[8 * i + j];
+                        u += (u >> 15) & Q as i16;
+                        t[j] = (((((u as u16) << 4) + Q as u16 / 2) / Q as u16) & 15) as u8;
+                    }
+                    self.data[k] = t[0] | (t[1] << 4);
+                    self.data[k + 1] = t[2] | (t[3] << 4);
+                    self.data[k + 2] = t[4] | (t[5] << 4);
+                    self.data[k + 3] = t[6] | (t[7] << 4);
+                    k += 4;
+                }
+            }
+            160 => {
+                for i in 0..N / 8 {
+                    for j in 0..8 {
+                        let mut u = poly.coeffs[8 * i + j];
+                        u += (u >> 15) & Q as i16;
+                        t[j] = (((((u as u32) << 4) + Q as u32 / 2) / Q as u32) & 31) as u8;
+                    }
+                    self.data[k] = t[0] | (t[1] << 5);
+                    self.data[k + 1] = (t[1] >> 3) | (t[2] << 2) | (t[3] << 7);
+                    self.data[k + 2] = (t[3] >> 1) | (t[4] << 4);
+                    self.data[k + 3] = (t[4] >> 4) | (t[5] << 1) | (t[6] << 6);
+                    self.data[k + 4] = (t[6] >> 2) | (t[7] << 3);
+                    k += 5;
+                }
+            }
+            _ => panic!("Invalid compressed poly bytes size."),
+        }
+    }
 }

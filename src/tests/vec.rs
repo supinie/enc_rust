cb60deb99c0eddd03ec38e450cb9023e963c96b4
supinie <86788874+supinie@users.noreply.github.com>
2023-08-22 17:03:35 +0100

    WIP: continue to refactor vec

--- src/tests/vec.rs
+++ src/tests/vec.rs
@@ -1,18 +1,20 @@
 #[cfg(test)]
 mod vec_tests {
-    use crate::{
-        vec::*,
-        params::*,
-        poly::*,
-    };
+    use crate::{params::*, poly::*, vec::*};
 
-    static test_params: [Params; 3] = [Params::sec_level_512, Params::sec_level_768, Params::sec_level_1024];
-
+    static test_params: [Params; 3] = [
+        Params::sec_level_512,
+        Params::sec_level_768,
+        Params::sec_level_1024,
+    ];
 
     #[test]
     fn add_test() {
-        let mut poly_vec1 = PolyVec { polynomials: vec![ Poly { coeffs: 20; N }, test_params[0].k ] };
-        let mut poly_vec2 = PolyVec { polynomials: vec![ Poly { coeffs: 30; N }, test_params[0].k ] };
-
+        let mut poly_vec1 = PolyVec {
+            polynomials: vec![ Poly { coeffs: 20; N }, test_params[0].k ],
+        };
+        let mut poly_vec2 = PolyVec {
+            polynomials: vec![ Poly { coeffs: 30; N }, test_params[0].k ],
+        };
     }
 }
--- src/vec.rs
+++ src/vec.rs
@@ -1,8 +1,4 @@
-use crate::{
-    poly::*,
-    params::*,
-    ntt::*,
-};
+use crate::poly::*;
 
 #[derive(Clone)]
 pub struct PolyVec {
@@ -10,39 +6,37 @@ pub struct PolyVec {
 }
 
 impl PolyVec {
-
     // Adds the given vector of polynomial and sets self to be the sum
     // Example:
     // vec1.add(vec2);
     pub fn add(&mut self, x: &PolyVec) {
         assert_eq!(self.polynomials.len(), x.polynomials.len());
-        for (i, _) in self.polynomials.iter().enumerate() {
+        for i in 0..self.polynomials.len() {
             self.polynomials[i].add(&x.polynomials[i]);
         }
     }
 
     pub fn reduce(&mut self) {
-        for (i, _) in self.polynomials.iter().enumerate() {
+        for i in 0..self.polynomials.len() {
             self.polynomials[i].reduce();
         }
     }
 
     pub fn normalise(&mut self) {
-        for (i, _) in self.polynomials.iter().enumerate() {
+        for i in 0..self.polynomials.len() {
             self.polynomials[i].reduce();
         }
     }
 
     pub fn ntt(&mut self) {
-        for (i, _) in self.polynomials.iter().enumerate() {
+        for i in 0..self.polynomials.len() {
             self.polynomials[i].ntt();
         }
     }
 
     pub fn inv_ntt(&mut self) {
-        for (i, _) in self.polynomials.iter().enumerate() {
+        for i in 0..self.polynomials.len() {
             self.polynomials[i].inv_ntt();
         }
     }
 }
-    

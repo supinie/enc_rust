2649b4ce9378ee5b181c0488679581806aa7bcad
supinie <86788874+supinie@users.noreply.github.com>
2023-08-22 17:15:04 +0100

    WIP: fix vec test syntax

--- src/tests/vec.rs
+++ src/tests/vec.rs
@@ -2,19 +2,19 @@
 mod vec_tests {
     use crate::{params::*, poly::*, vec::*};
 
-    static test_params: [Params; 3] = [
-        Params::sec_level_512,
-        Params::sec_level_768,
-        Params::sec_level_1024,
+    static TEST_PARAMS: [Params; 3] = [
+        Params::sec_level_512(),
+        Params::sec_level_768(),
+        Params::sec_level_1024(),
     ];
 
     #[test]
     fn add_test() {
         let mut poly_vec1 = PolyVec {
-            polynomials: vec![ Poly { coeffs: 20; N }, test_params[0].k ],
+            polynomials: vec![ Poly { coeffs: [20; N] }; TEST_PARAMS[0].k ],
         };
         let mut poly_vec2 = PolyVec {
-            polynomials: vec![ Poly { coeffs: 30; N }, test_params[0].k ],
+            polynomials: vec![ Poly { coeffs: [30; N] }; TEST_PARAMS[0].k ],
         };
     }
 }

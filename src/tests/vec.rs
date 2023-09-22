7cbe1b3b5954478903a46e292c56c20ed1781522
supinie <86788874+supinie@users.noreply.github.com>
2023-09-22 11:42:29 +0100

    finished vec tests

--- src/tests/vec.rs
+++ src/tests/vec.rs
@@ -1,6 +1,6 @@
 #[cfg(test)]
 mod vec_tests {
-    use crate::{params::*, poly::*, vec::*};
+    use crate::{params::*, poly::*, vec::*, field_ops::*};
 
     static TEST_PARAMS: [Params; 3] = [
         Params::sec_level_512(),
@@ -32,6 +32,47 @@ mod vec_tests {
             };
             poly_vec.reduce();
             assert_eq!(poly_vec.polynomials, vec![ Poly { coeffs: [2806; N] }; sec_level.k]);
+        }
+    }
+
+    #[test]
+    fn normalise_test() {
+        for sec_level in TEST_PARAMS.iter() {
+            let mut poly_vec = PolyVec {
+                polynomials: vec![ Poly { coeffs: [i16::MAX; N] }; sec_level.k ],
+            };
+            poly_vec.normalise();
+            assert_eq!(poly_vec.polynomials, vec![ Poly { coeffs: [cond_sub_q(barrett_reduce(i16::MAX)); N] }; sec_level.k ]);
+        }
+    }
+
+
+    #[test]
+    fn ntt_invntt_test() {
+        for sec_level in TEST_PARAMS.iter() {
+            let mut input_vec = PolyVec {
+                polynomials: vec![ Poly {coeffs: [20; N] }; sec_level.k ],
+            };
+            let mut original_input = input_vec.clone();
+            original_input.normalise();
+            
+            input_vec.ntt();
+            input_vec.normalise();
+            input_vec.inv_ntt();
+            input_vec.normalise();
+
+            for i in 0..sec_level.k {
+                for j in 0..N {
+                    let p: i32 = input_vec.polynomials[i].coeffs[j] as i32;
+                    let q: i32 = original_input.polynomials[i].coeffs[j] as i32;
+                    assert_eq!(
+                        p,
+                        (q * (1 << 16)) % (Q as i32),
+                        "we are testing equality with original in poly {}, index {}",
+                        i, j
+                    );
+                }
+            }
         }
     }
 }

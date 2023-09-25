8b27b13977c4b05e2d5b58be759d6381cea22513
supinie <86788874+supinie@users.noreply.github.com>
2023-09-25 12:06:42 +0100

    add docstrings

--- src/sample.rs
+++ src/sample.rs
@@ -4,6 +4,10 @@ use sha3::{digest::{Update, ExtendableOutput, XofReader}, Shake256};
 use byteorder::{ByteOrder, LittleEndian};
 
 impl Poly {
+    // Sample our polynomial from a centered binomial distribution
+    // n = 4, p = 1/2
+    // ie. coefficients are in {-2, -1, 0, 1, 2}
+    // with probabilities {1/16, 1/4, 3/8, 1/4, 1/16}
     pub fn derive_noise_2(&mut self, seed: &[u8], nonce: u8) {
         let key_suffix: [u8; 1] = [nonce];
         let mut h = Shake256::default();
@@ -30,6 +34,11 @@ impl Poly {
         }
     }
 
+    
+    // Sample our polynomial from a centered binomial distribution
+    // n = 6, p = 1/2
+    // ie. coefficients are in {-3, -2, -1, 0, 1, 2, 3}
+    // with probabilities {1/64, 3/32, 15/64, 5/16, 15/64, 3/32, 1/64}
     pub fn derive_noise_3(&mut self, seed: &[u8], nonce: u8) {
         let key_suffix: [u8; 1] = [nonce];
         let mut h = Shake256::default();

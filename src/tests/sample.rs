346b43f4a68a86757c580dd2072191dc69588fe7
supinie <86788874+supinie@users.noreply.github.com>
2023-09-25 18:42:52 +0100

    finish dist test and add helper funcs

--- src/tests/sample.rs
+++ src/tests/sample.rs
@@ -52,6 +52,38 @@ mod sample_tests {
 
 
     #[test]
+    fn derive_noise_2_dist_test() {
+        let expected_probabilities: HashMap<i16, f64> = [
+            (-2, 1.0 / 16.0),
+            (-1, 1.0 / 4.0),
+            (0, 3.0 / 8.0),
+            (1, 1.0 / 4.0),
+            (2, 1.0 / 16.0),
+        ].iter().cloned().collect();
+
+        let mut poly = Poly::new();
+        let seed = generate_random_seed();
+        let nonce = generate_random_nonce();
+
+        poly.derive_noise_2(&seed, nonce);
+
+        let mut actual_counts: HashMap<i16, usize> = HashMap::new();
+        for &coeff in poly.coeffs.iter() {
+            *actual_counts.entry(coeff).or_insert(0) += 1;
+        }
+
+        for (coeff, expected_prob) in expected_probabilities.iter() {
+            let actual_count = *actual_counts.get(coeff).unwrap_or(&0);
+            let total_samples = poly.coeffs.len() as f64;
+            let actual_prob = (actual_count as f64) / total_samples;
+
+
+            assert!((actual_prob - expected_prob).abs() < EPSILON, "Actual probability {} does not match expected {} within boundries for coeff {}", actual_prob, expected_prob, coeff);
+        }
+    }
+
+
+    #[test]
     fn derive_noise_3_dist_test() {
         let expected_probabilities: HashMap<i16, f64> = [
             (-3, 1.0 / 64.0),

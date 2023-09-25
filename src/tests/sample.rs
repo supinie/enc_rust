79fe7529ed77f351a2d30c528eefd8ae05657164
supinie <86788874+supinie@users.noreply.github.com>
2023-09-25 18:40:33 +0100

    add eta 3 dist test

--- src/tests/sample.rs
+++ src/tests/sample.rs
@@ -1,13 +1,33 @@
 #[cfg(test)]
 mod sample_tests {
     use crate::{params::*, poly::*};
-    use std::ops::Range;
+    use std::{ops::Range, collections::HashMap};
+    use rand::{Rng, SeedableRng};
+    use rand::rngs::StdRng;
+
+    const EPSILON: f64 = 0.1;
+
+    fn generate_random_seed() -> [u8; 32] {
+        let mut rng = StdRng::from_entropy();
+        let mut seed = [0u8; 32];
+        rng.fill(&mut seed);
+        seed
+    }
+
+    fn generate_random_nonce() -> u8 {
+        let mut rng =StdRng::from_entropy();
+        rng.gen::<u8>()
+    }
 
     #[test]
-    fn derive_noise_2_test() {
+    fn derive_noise_2_range_test() {
         let range: Range<i16> = -2..3;
         let mut poly = Poly::new();
-        poly.derive_noise_2(&[8], 8);
+        let seed = generate_random_seed();
+        let nonce = generate_random_nonce();
+
+        poly.derive_noise_2(&seed, nonce);
+
         for coeff in poly.coeffs.iter() {
             println!("{}", coeff);
             assert!(range.contains(coeff), "coefficient {} not in valid range", coeff);
@@ -16,13 +36,51 @@ mod sample_tests {
 
 
     #[test]
-    fn derive_noise_3_test() {
+    fn derive_noise_3_range_test() {
         let range: Range<i16> = -3..4;
         let mut poly = Poly::new();
-        poly.derive_noise_3(&[8], 8);
+        let seed = generate_random_seed();
+        let nonce = generate_random_nonce();
+        
+        poly.derive_noise_3(&seed, nonce);
+
         for coeff in poly.coeffs.iter() {
             println!("{}", coeff);
             assert!(range.contains(coeff), "coefficient {} not in valid range", coeff);
+        }
+    }
+
+
+    #[test]
+    fn derive_noise_3_dist_test() {
+        let expected_probabilities: HashMap<i16, f64> = [
+            (-3, 1.0 / 64.0),
+            (-2, 3.0 / 32.0),
+            (-1, 15.0 / 64.0),
+            (0, 5.0 / 16.0),
+            (1, 16.0 / 64.0),
+            (2, 3.0 / 32.0),
+            (3, 1.0 / 64.0),
+        ].iter().cloned().collect();
+
+        let mut poly = Poly::new();
+        let seed = generate_random_seed();
+        let nonce = generate_random_nonce();
+
+        poly.derive_noise_3(&seed, nonce);
+
+        let mut actual_counts: HashMap<i16, usize> = HashMap::new();
+        for &coeff in poly.coeffs.iter() {
+            *actual_counts.entry(coeff).or_insert(0) += 1;
+        }
+
+        for (coeff, expected_prob) in expected_probabilities.iter() {
+            let actual_count = *actual_counts.get(coeff).unwrap_or(&0);
+            let total_samples = poly.coeffs.len() as f64;
+            let actual_prob = (actual_count as f64) / total_samples;
+
+
+            assert!((actual_prob - expected_prob).abs() < EPSILON, "Actual probability {} does not match expected {} within boundries for coeff {}", actual_prob, expected_prob, coeff);
         }
     }
 }

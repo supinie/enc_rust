b3a815e2d5c84cec155e85eaaff13cba2b215989
supinie <86788874+supinie@users.noreply.github.com>
2023-09-28 11:28:51 +0100

    clippy lints

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,22 @@
 // REMOVE BEFORE FINAL, DEVELOPMENT ONLY
 #![forbid(unsafe_code)]
 
+#![warn(
+    clippy::cast_lossless,
+    clippy::cast_possible_truncation,
+    clippy::cast_possible_wrap,
+    clippy::cast_precision_loss,
+    clippy::cast_sign_loss,
+    clippy::checked_conversions,
+    clippy::implicit_saturating_sub,
+    clippy::panic,
+    clippy::panic_in_result_fn,
+    clippy::unwrap_used,
+    rust_2018_idioms,
+    unused_lifetimes,
+    unused_qualifications
+)]
+
 mod buffer;
 mod field_ops;
 mod indcpa;

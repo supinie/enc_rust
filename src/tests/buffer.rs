34c54811180d2f274ebd450b3fa16182f6392ed5
supinie <86788874+supinie@users.noreply.github.com>
2024-01-17 14:09:44 +0000

    pack and unpack vec test
    pack and unpack vec test 512
    
    add other sec levels

--- src/tests/buffer.rs
+++ src/tests/buffer.rs
@@ -1,7 +1,7 @@
 #![allow(warnings)]
 #[cfg(test)]
 pub(in crate::tests) mod buffer_tests {
-    use crate::{params::*, polynomials::*};
+    use crate::{params::*, polynomials::*, vectors::*};
     use rand::Rng;
     extern crate std;
     use std::vec::Vec;
@@ -31,19 +31,19 @@ pub(in crate::tests) mod buffer_tests {
     }
 
     #[test]
-    fn pack_unpack_test() {
-        let p = Poly { coeffs: [20; N] };
-        let mut buffer = [0; 3 * 128];
-        &p.pack(&mut buffer);
+    fn pack_unpack_poly_test() {
+        let poly = Poly { coeffs: [20; N] };
+        let mut buffer = [0; POLYBYTES];
+        &poly.pack(&mut buffer);
 
-        let mut comp_p = Poly::new();
-        comp_p.unpack(&buffer);
+        let mut comp_poly = Poly::new();
+        comp_poly.unpack(&buffer);
 
-        assert_eq!(comp_p.coeffs, p.coeffs);
+        assert_eq!(comp_poly.coeffs, poly.coeffs);
     }
 
     #[test]
-    fn compress_decompress_test() {
+    fn compress_decompress_poly_test() {
         for sec_level in TEST_PARAMS.iter() {
             let buf = generate_random_buffer(sec_level.poly_compressed_bytes());
             let mut buf_comp = zero_initialise_buffer(sec_level.poly_compressed_bytes());
@@ -54,8 +54,43 @@ pub(in crate::tests) mod buffer_tests {
             poly.compress(&mut buf_comp, sec_level);
 
             assert_eq!(buf_comp, buf);
-            assert_eq!(buf_comp, buf);
-            assert_eq!(buf_comp, buf);
+        }
+    }
+
+    #[test]
+    fn pack_unpack_vec_test() {
+        let poly = Poly { coeffs: [20; N] };
+        for sec_level in TEST_PARAMS.iter() {
+            if let &SecurityLevel::FiveOneTwo { .. } = sec_level {
+                let mut buffer = [0; 2 * POLYBYTES];
+                let poly_vec = PolyVec512::from([poly; 2]);
+                &poly_vec.pack(&mut buffer);
+
+                let mut comp_poly_vec = PolyVec512::from([Poly::new(); 2]);
+                comp_poly_vec.unpack(&buffer);
+
+                assert_eq!(comp_poly_vec, poly_vec);
+            }
+            if let &SecurityLevel::SevenSixEight { .. } = sec_level {
+                let mut buffer = [0; 3 * POLYBYTES];
+                let poly_vec = PolyVec768::from([poly; 3]);
+                &poly_vec.pack(&mut buffer);
+
+                let mut comp_poly_vec = PolyVec768::from([Poly::new(); 3]);
+                comp_poly_vec.unpack(&buffer);
+
+                assert_eq!(comp_poly_vec, poly_vec);
+            }
+            if let &SecurityLevel::TenTwoFour { .. } = sec_level {
+                let mut buffer = [0; 4 * POLYBYTES];
+                let poly_vec = PolyVec1024::from([poly; 4]);
+                &poly_vec.pack(&mut buffer);
+
+                let mut comp_poly_vec = PolyVec1024::from([Poly::new(); 4]);
+                comp_poly_vec.unpack(&buffer);
+
+                assert_eq!(comp_poly_vec, poly_vec);
+            }
         }
     }
 }

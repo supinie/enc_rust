81c5bead0607ab8d3c503c8836e77e84f045d54e
supinie <86788874+supinie@users.noreply.github.com>
2024-01-19 13:52:56 +0000

    add matrix tests

---
+++ src/tests/matrix.rs
@@ -0,0 +1,10 @@
+#[cfg(test)]
+mod matrix_tests {
+    use crate::{matrix::*, params::{SecurityLevel, K}};
+
+    static TEST_PARAMS: [SecurityLevel; 3] = [
+        SecurityLevel::new(K::Two),
+        SecurityLevel::new(K::Three),
+        SecurityLevel::new(K::Four),
+    ];
+}

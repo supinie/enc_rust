6909d300266d60626fe5a1a0633be2ac47bc84ac
supinie <86788874+supinie@users.noreply.github.com>
2024-01-19 14:49:55 +0000

    testing

--- src/tests/matrix.rs
+++ src/tests/matrix.rs
@@ -2,7 +2,7 @@
 mod matrix_tests {
     use crate::{
         matrix::*,
-        params::{SecurityLevel, K},
+        params::{SecurityLevel, K, N}, polynomials::Poly, vectors::PolyVec512,
     };
 
     static TEST_PARAMS: [SecurityLevel; 3] = [
@@ -10,4 +10,12 @@ mod matrix_tests {
         SecurityLevel::new(K::Three),
         SecurityLevel::new(K::Four),
     ];
+
+
+    // #[test]
+    // fn transpose_test() {
+        // let mut matrix: Mat512 = [PolyVec512::from([Poly { coeffs: [1; N] }, Poly {coeffs: [2; N] }]); 2];
+        // matrix.transpose();
+        // assert_eq!(matrix, [PolyVec512::from([Poly { coeffs: [1; N]}; 2]), PolyVec512::from([Poly { coeffs: [2; N]}; 2])]);
+    // }
 }

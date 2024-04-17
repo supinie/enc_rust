eee57c8bd92a9f5fb35abe847dfb4c30c8331f32
supinie <86788874+supinie@users.noreply.github.com>
2024-04-17 19:05:30 +0100

    TODO: fix inv_ntt

--- src/tests/ntt.rs
+++ src/tests/ntt.rs
@@ -4,6 +4,7 @@
 pub(in crate::tests) mod ntt_tests {
     use crate::{params::*, polynomials::*, tests::polynomials::poly_tests::*};
     use proptest::prelude::*;
+    use more_asserts::{assert_le, assert_ge};
 
     proptest! {
         #[test]
@@ -12,6 +13,46 @@ pub(in crate::tests) mod ntt_tests {
             let output_2 = poly.mont_form().ntt();
             let output_3 = poly.barrett_reduce().ntt();
         }
+
+        #[test]
+        fn ntt_test_alt(poly in new_ntt_poly()) {
+            let comp_poly = poly.normalise();
+
+            poly.normalise()
+                .ntt()
+                .coeffs()
+                .iter()
+                .for_each(|&coeff| {
+                    assert_le!(coeff, (7 * Q) as i16);
+                    assert_ge!(coeff, -((7 * Q) as i16));
+                });
+
+            poly.normalise()
+                .ntt()
+                .barrett_reduce()
+                .normalise()
+                .inv_ntt()
+                .coeffs()
+                .iter()
+                .for_each(|&coeff| {
+                    assert_le!(coeff, Q as i16);
+                    assert_ge!(coeff, -(Q as i16));
+                });
+
+            poly.normalise()
+                .ntt()
+                .barrett_reduce()
+                .normalise()
+                .inv_ntt()
+                // .barrett_reduce()
+                // .normalise()
+                .coeffs()
+                .iter()
+                .for_each(|&coeff| {
+                    assert_eq!(coeff as i32, ((coeff as i32) * (1 << 16)) % (Q as i32));
+                });
+        }
+
 
         #[test]
         fn inv_ntt_test(poly in new_ntt_poly()) {
